9e962e89bcaaa6e3a3d4982ada5bb77d531535b6
Mazen Hany <mazenhany1912003@gmail.com>
2025-12-06 23:06:40 +0200

    test openai functionality

--- src/main.py
+++ src/main.py
@@ -1,5 +1,28 @@
 from fastapi import FastAPI
 from routes.base_route import base_route
+from utils import Settings ,get_settings
+from controllers import Factory
+import logging
+from openai import OpenAI
+logging.basicConfig(
+    level=logging.INFO,
+    filename='log.log',
+    filemode='w',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+config = get_settings()
 
-app = FastAPI()
-app.include_router(base_route)
+factory = Factory(config)
+
+llm = factory.create(config.LLM_BACKEND)
+llm.set_generation_model(llm_model_id=config.LLM_MODEL)
+
+
+embedding = factory.create(config.EMBEDDING_MODEL_BACKEND)
+embedding.set_embedding_model(embedding_model_id=config.EMBEDDING_MODEL_NAME
+                        ,embedding_dim=config.EMBEDDING_MODEL_DIM)
+
+
+print(embedding.embed_text("What is the Machine Learning in 50 word???"))
+print(llm.generate_text("What is the Machine Learning in 50 word???" , history=[]))
+
