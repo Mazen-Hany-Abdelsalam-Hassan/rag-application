ded2921277cf8ccaa604baaac9382e6e827d50f6
Mazen Hany <mazenhany1912003@gmail.com>
2025-12-06 23:05:17 +0200

    introduce new env variable

--- src/utils/load_env.py
+++ src/utils/load_env.py
@@ -1,8 +1,25 @@
 from pydantic_settings import BaseSettings , SettingsConfigDict
 from typing import Optional
+
 class Settings(BaseSettings):
     VERSION:str
-    APP_NAME:str    
+    APP_NAME:str 
+
+    OPENAI_KEY:Optional[str] = "OLLAMA"
+    OPENAI_URL:str
+
+    TEMPREATURE:float
+    MAX_OUTPUT_TOKEN:int
+    MAX_INPUT_TOKEN:int
+
+    LLM_BACKEND:str
+    EMBEDDING_MODEL_BACKEND:str
+    
+    LLM_MODEL:str
+    EMBEDDING_MODEL_NAME:str
+    EMBEDDING_MODEL_DIM:int
+
+
     class Config:
         env_file = '.env'
 def get_settings():
