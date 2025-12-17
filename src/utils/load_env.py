1932e7b742b2791772b74456b8ce4e87d4c3ea9a
Mazen <mazenhany1912003@gmail.com>
2025-12-17 19:28:12 +0200

    add_new_env_variable

--- src/utils/load_env.py
+++ src/utils/load_env.py
@@ -1,13 +1,16 @@
 from pydantic_settings import BaseSettings , SettingsConfigDict
-from typing import Optional
+from typing import Optional ,List
 
 class Settings(BaseSettings):
     VERSION:str
     APP_NAME:str 
+    ALLOWED_FILE_TYPES:List[str]
+    MAXIMUM_ALLOWED_SIZE:int ## 5MIB of data 
+
 
     OPENAI_KEY:Optional[str] = "OLLAMA"
     OPENAI_URL:str
-
+    CHUNK_SIZE:int
     TEMPREATURE:float
     MAX_OUTPUT_TOKEN:int
     MAX_INPUT_TOKEN:int
@@ -18,6 +21,7 @@ class Settings(BaseSettings):
     LLM_MODEL:str
     EMBEDDING_MODEL_NAME:str
     EMBEDDING_MODEL_DIM:int
+
 
 
     class Config:
