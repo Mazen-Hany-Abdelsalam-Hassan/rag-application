6715a99fa4d8322155b54bdc92fef7bbe8961705
Mazen Hany <mazenhany1912003@gmail.com>
2025-12-20 14:22:37 +0200

    Enhance the code readability

--- src/models/enumeration/data_controller_enum.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class DataControllerEnum(Enum):
-    FILE_SIZE_EXCEDED="The file size Exceded"
-    FILE_TYPE_NOT_SUPPORTED= "File type not supported"
-    FILE_UPLOADED_SUCCESSFULLY = "File uploaded successfully"
-    FILE_NOT_UPLOADED_SUCCESSFULLY = "File not uploaded successfully"
