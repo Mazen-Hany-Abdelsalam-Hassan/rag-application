2faf2c21a1f482c29b3364e7fc07f46a98d83dd5
Mazen Hany <mazenhany1912003@gmail.com>
2025-12-21 17:44:57 +0200

    modeify data route

--- src/routes/data_route.py
+++ src/routes/data_route.py
@@ -1,31 +1,47 @@
-from fastapi import APIRouter ,status,UploadFile
+from fastapi import APIRouter ,status,UploadFile , Request
 from fastapi.responses import JSONResponse 
 from utils import Settings 
 from controllers import DataLoadingController ,DataProcessingController
 import aiofiles
 import os
-from models import ProcessResponse ,ResponseEnum
+import hashlib
+from models import ProcessResponse ,ResponseEnum,UserModel
 DataRoute =APIRouter(prefix="/Rag", 
                            tags=["welcome", "rag"])
 @DataRoute.post("/upload_file/{User}")
-async def file_upload(User:str, file:UploadFile):
+async def file_upload(request:Request,User:str, file:UploadFile):
     data_controller = DataLoadingController(User)
     chunk_size = data_controller.environment_variable.CHUNK_SIZE
-    file_name =  data_controller.is_valid_file(file)
-    
-    if file_name: 
-        file_path = os.path.join(
-        data_controller.user_directory,file_name)
+    file_id =  data_controller.is_valid_file(file)
+    hasher = hashlib.sha256()
+    if file_id: 
+        file_path = os.path.join(data_controller.user_directory,
+                                 file_id)
+        i = 0
+        db_files = request.app.Database
+        user_model = UserModel(database_client=db_files)
         async with aiofiles.open(file_path,'wb') as f :
             while True:
                 chunk = await file.read(chunk_size)
                 if not chunk:
                     break
+                if  i == 0:
+                   hasher.update(chunk)
+                   file_hash = str(hasher.hexdigest())
+                   i+=1
+                   user,exists = await user_model.insert_user_text_hash(
+                                                                username=User,
+                                                                text_encoding=file_hash,
+                                                                file_id=file_id)
+                   
+                if exists :
+                    os.remove(file_path)
+                    break
                 await f.write(chunk)
-
+        print(await user_model.add_chunk_size_and_overlap("Mezo","albama_n9VkRJlv.txt",200,50))
         
         return JSONResponse(content={"message": ResponseEnum.FILE_UPLOADED_SUCCESSFULLY.value,
-                                     "process_id":file_name})
+                                     "process_id":user.file_id})
     
     return JSONResponse(content={"message": ResponseEnum.FILE_NOT_UPLOADED_SUCCESSFULLY.value} 
                         ,status_code=status.HTTP_400_BAD_REQUEST)
