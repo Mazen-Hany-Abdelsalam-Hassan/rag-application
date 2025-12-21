f01a3d70124ea6a5f792cee6a6c10eca24d9c054
Mazen Hany <mazenhany1912003@gmail.com>
2025-12-21 17:41:36 +0200

    add start event and mong connection

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routes import BaseRoute , DataRoute
 from utils import Settings ,get_settings
+from motor.motor_asyncio import AsyncIOMotorClient
+from models import UserModel
 
-app= FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    env_var = get_settings()
+    app.MongoClient =AsyncIOMotorClient(env_var.MONGO_DB_CLIENT)
+    app.Database = app.MongoClient[env_var.MONGO_DB_NAME]
+    
+    yield
+    app.MongoClient.close()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(BaseRoute)
 app.include_router(DataRoute)
