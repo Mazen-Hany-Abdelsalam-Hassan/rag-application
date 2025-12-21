97c501a4c3df84d0644fb5484b54b8cf4b3726a8
Mazen Hany <mazenhany1912003@gmail.com>
2025-12-21 17:44:43 +0200

    update user schema

--- src/models/db_schema/user_schema.py
+++ src/models/db_schema/user_schema.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel ,Field
 from bson.objectid import ObjectId
-from typing import Literal
-from ..enumeration import Status
+from typing import Literal,Optional
+from ..enumeration import FileStatus
 class UserSchema(BaseModel):
     """
     This Schema contains every details related to any upload transaction 
@@ -10,10 +10,10 @@ class UserSchema(BaseModel):
     id:ObjectId=Field(None , alias="_id") ##
     username:str
     file_id:str
-    status:Status
+    status:str
     text_encoding:str ## for deduplication
-    chunk_size:int
-    chunk_overlap:int
+    chunk_size:Optional[int]=None
+    chunk_overlap:Optional[int]=None
     parsing_method:Literal["Naive"]= Field(default="Naive")
 
     class Config:
